58fb93f0f660e99a4a79171c6dfcad77844a6001
Alex.Liu <larch.liu@gmail.com>
2025-05-07 22:36:56 +0800

    chore: change prompt

--- server/api/scene-words.post.ts
+++ server/api/scene-words.post.ts
@@ -23,7 +23,7 @@ const SceneWords = z.object({
   })),
 })
 
-const prompt = `你是一位精通中英语言的翻译官，当用户输入一个中文场景描述后需要解析该场景下可能使用到的英文 Vocabulary，根据vocabulary 给出可能会用到的Phrases，最后给出tips，用于指出在该场景使用词汇，短语时需要注意的语法格式问题，并给出示例，示例给出 explain。`
+const prompt = `你是一位精通中英语言的翻译官，当用户输入一个中文场景描述后需要解析该场景下可能使用到的英文 Vocabulary，根据vocabulary 给出可能会用到的Phrases，最后给出tips，用于指出在该场景使用词汇，短语时需要注意的语法格式问题，并给出示例，示例基于英文用法结合中英文给出 explain。`
 
 export default eventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig()
